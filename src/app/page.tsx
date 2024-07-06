31b9f80ade2e0e07d2ca45f20e7d6b6ff95b1893
renhick <96395123+renhick@users.noreply.github.com>
2024-07-06 13:57:18 +0200

    CXC Update 060720204

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Extras from "@/components/Extras";
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Extras></Extras>
 
       <SpeedInsights />
+      <Analytics/>
     </>
   );
 }
